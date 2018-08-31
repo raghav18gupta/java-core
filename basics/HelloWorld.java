9d751e1f864762d4d64840d45ca366536895946b
raghav18gupta <18raghavgupta@gmail.com>
2018-08-31 18:16:40 +0530

    commit

--- basics/HelloWorld.java
+++ basics/HelloWorld.java
@@ -15,4 +15,9 @@ void: 	The main method doesn't return anything.
 main(): Name configured in the JVM.
 String[]: The main method accepts a single argument: 
         an array of elements of type String.
+
+System is a predefined class that provides access to the system, and out is 
+the variable of type output stream that is connected to the console.
+
+
 */
