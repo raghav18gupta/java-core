307e3df7447b11119686fdc1ea288a71fc4100bd
raghav18gupta <18raghavgupta@gmail.com>
2018-08-31 17:49:31 +0530

    commit

--- basics/HelloWorld.java
+++ basics/HelloWorld.java
@@ -5,3 +5,14 @@ class HelloWorld
 		System.out.println("Hello World!");
 	}
 }
+
+
+/*
+public:	So that JVM can execute the method from anywhere.
+static:	Main method is to be called without object. 
+		The modifiers public and static can be written in either order.
+void: 	The main method doesn't return anything.
+main(): Name configured in the JVM.
+String[]: The main method accepts a single argument: 
+        an array of elements of type String.
+*/
